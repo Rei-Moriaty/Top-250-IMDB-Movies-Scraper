155dc7a7bf31060068db3fa968e161aabef4555b
Rei-Moriaty <38129763+Rei-Moriaty@users.noreply.github.com>
2019-11-25 10:24:51 +0530

    Create python scraper

--- imdb_movie_scrapper.py
+++ imdb_movie_scrapper.py
@@ -5,6 +5,8 @@ driver = Firefox()
 movie_links = [] 
 data = {}
 data['movie'] = []
+MAX_NO_FILMS = 250
+imdb_url = 'https://www.imdb.com/chart/top?ref_=nv_mv_250'
 class Scarper:
     def __init__(self, url):
         self.url = url
@@ -32,22 +34,33 @@ class Scarper:
             #extract the title
             movie['title'] = movie_details.find('div', {'class' : 'title_wrapper'}).find('h1').text
             #extract the directors
-            movie_details_list = movie_details.find('div', {'class' : 'plot_summary'}).findAll('div', {'class' : 'credit_summary_item'})
-            director_list = movie_details_list[0].findAll('a')
+            #movie_details_list = movie_details.find('div', {'class' : 'plot_summary'}).findAll('div', {'class' : 'credit_summary_item'})[2].find('a')[3]['href']
+            #see_full_cast_url = movie_details.find('div', {'class' : 'titleCast'}).find('div', {'class' : 'see-more'}).find('a')['href']
+            see_full_cast_url = movie_links[i].split('?')[0] + movie_details.find('div', {'class' : 'plot_summary'}).findAll('div', {'class' : 'credit_summary_item'})[2].findAll('a')[3]['href']
+            see_full_cast_response = driver.request('GET', 'https://www.imdb.com/'+see_full_cast_url)
+            see_full_cast_details = bs(see_full_cast_response.text, 'lxml')
+            full_cast_details = see_full_cast_details.find('div', {'id' : 'fullcredits_content', 'class' : 'header'}).findAll('table', {'class' : 'simpleTable simpleCreditsTable'})
+
+            director_list = full_cast_details[0].findAll('tr')
             movie['director'] = []
             for director in director_list:
-                movie['director'].append(director.text)
+                movie['director'].append(director.find('td', {'class' : 'name'}).find('a').text.strip())
             #extract the writers
-            writer_list = movie_details_list[1].findAll('a')
+            writer_list = full_cast_details[1].findAll('tr')
             movie['writer'] = []
             for writer in writer_list:
-                movie['writer'].append(writer.text)
+                if writer.find('td', {'class' : 'name'}) != None:
+                    movie['writer'].append(writer.find('td', {'class' : 'name'}).find('a').text.strip())
             #extract the stars
-            star_list = movie_details_list[2].findAll('a')
+            star_list = see_full_cast_details.find('div', {'id' : 'fullcredits_content'}).find('table', {'class' : 'cast_list'}).findAll('tr')
             movie['star'] = []
+            '''
             for star in star_list:
-                if not star.text.__contains__('See full cast & crew'):
-                    movie['star'].append(star.text)
+                if not star.find('td')['class'].__contains__('castlist_label'):
+                    movie['star'].append(star.findAll('td')[1].find('a').text.strip())
+            '''
+            for j in range(1, 4):
+                movie['star'].append(star_list[j].findAll('td')[1].find('a').text.strip())
             #extract poster url
             movie['poster'] = movie_details.find('div', {'class' : 'poster'}).find('img')['src']
             #extrcat rating
@@ -65,7 +78,7 @@ class Scarper:
             data['movie'].append(movie)
 
 #Create scraper object and initialize with url
-scraper = Scarper('https://www.imdb.com/chart/top?ref_=nv_mv_250')
+scraper = Scarper(imdb_url)
 #send request to the giver url
 scraper.load_url()
 #get all links of each movie
